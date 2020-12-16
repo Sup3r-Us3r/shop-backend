a71e5d4011a0d1b1855a9395a7af0c9115d76a14
Sup3r-Us3r <maydersonmello@gmail.com>
2020-12-15 21:45:05 -0300

    updated routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
 import userRouter from './user.routes';
+import categoryRouter from './category.routes';
+import productRouter from './product.routes';
 
 const routes = Router();
 
 routes.use('/user', userRouter);
+routes.use('/category', categoryRouter);
+routes.use('/product', productRouter);
 
 export default routes;
