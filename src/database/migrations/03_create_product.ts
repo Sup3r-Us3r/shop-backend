729a0a68b4aed9d187f9a44ab3d825ef07671f76
Sup3r-Us3r <maydersonmello@gmail.com>
2020-12-25 14:54:03 -0300

    created new fields

--- src/database/migrations/03_create_product.ts
+++ src/database/migrations/03_create_product.ts
@@ -3,8 +3,10 @@ import Knex from 'knex';
 export async function up(knex: Knex) {
   return knex.schema.createTable('tb_product', table => {
     table.increments('id');
+    table.boolean('star').notNullable().defaultTo(false);
     table.string('name').notNullable();
     table.float('price', 2).notNullable();
+    table.integer('amount').notNullable();
     table.string('category').notNullable();
     table.text('description').notNullable();
     table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
